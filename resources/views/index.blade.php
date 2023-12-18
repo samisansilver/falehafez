9572e7484d0c9eebc9214c3ebe533798297fe579
Saman Mashayekhi <sami.sansilver10@gmail.com>
2023-12-18 12:13:09 +0330

    change font size button

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,6 +2,6 @@
 @section('button')
 <form action="/fal" method="post">
     @csrf
-    <button style="background: #00ae8e; color: white; width: 40%; height: 50px; border-radius: 10px; font-size: 20px" class="font-title saman-btn mb-3" type="submit">فالت رو ببین</button>
+    <button style="background: #00ae8e; color: white; width: 40%; height: 50px; border-radius: 10px; font-size: large" class="font-title saman-btn mb-3" type="submit">فالت رو ببین</button>
 </form>
 @endsection
